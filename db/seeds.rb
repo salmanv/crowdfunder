ac60baa5618bc21184ceb6ab61be235f11224649
Salman Vajid <s@vajid.com>
2013-09-30 17:00:23 -0400

    Seeding db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#destroy all pre-existing data before seeding 
+
+User.destroy_all
+Project.destroy_all
+
+user = User.create!(first_name: "john", last_name: "Doe", email: "john@doe.com", password: "password")
+
+50.times do |i|
+	project1 = user.projects.create!(title: "Project #{i}", teaser: "Teaser text #{i}",
+	 description: "description #{i}", goal: 13000)
+	
+end
+
