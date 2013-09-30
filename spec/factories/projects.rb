721610249cdbfad8fb7c3038e48a70c2ef39ca88
Salman Vajid <s@vajid.com>
2013-09-30 17:12:22 -0400

    Generating factories of Projects and Users

---
+++ spec/factories/projects.rb
@@ -0,0 +1,14 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :project do
+  	user
+  	title "Reusing wasted water"
+  	teaser "Diamonds are the most valueble asset, but the most useless, whereas water is the most valueble and useful"
+  	description "It is described as a machine that can reuse water that has gone to waste"
+  	goal 35000000
+ 
+
+
+  end
+end
