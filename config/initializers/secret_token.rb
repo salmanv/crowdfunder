70fe939ce14d329af8a8a47147ddb85c94c36a57
Salman Vajid <s@vajid.com>
2013-09-30 12:27:56 -0400

    adding all rails default apps

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crowdfunder::Application.config.secret_token = '58500e244717ea8f1481a91f12625e9680466129e978284d0f3378b5c7e804240a5283c0221e54cdfac0fc3e4cc14e68e9a917ad246d0a29f64c3618a1e02caa'
