f68c88ef8d084db3614875fcf82a202b9a521620
Salman Vajid <s@vajid.com>
2013-10-01 15:25:01 -0400

    updated current_path = project_path

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@ Project.destroy_all
 user = User.create!(first_name: "john", last_name: "Doe", email: "john@doe.com", password: "password")
 
 50.times do |i|
-	project1 = user.projects.create!(title: "Project #{i}", teaser: "Teaser text #{i}",
-	 description: "description #{i}", goal: 13000)
+	project1 = user.projects.create!(title: "Project #{i+1}", teaser: "Teaser text #{i+1}",
+	 description: "description #{i+1}", goal: 13000)
 	
 end
 
--- spec/features/project_flows_spec.rb
+++ spec/features/project_flows_spec.rb
@@ -32,13 +32,14 @@ describe "Project Listing" do
       page.should have_content('Project 3')
       expect(page).to have_content('Project 3')
 
-      #click a link to the project1's show page
+      # Click a link to the project1's show page
       click_link('Project 1')
-      #expect we're on project 1's show page
-      # expect(current_path).to eq(projects_path(project1))
-      #Expect on this page the first h1 has the text project1's title
-      expect(page).to have_selector('h1:first', text: project1.title);
-    
+      # Expect we're on project1's show page
+      expect(current_path).to eq(project_path(project1))
+      # Expect on this page the first h1 has the text project1's title
+      page.should have_selector('h1:first', text: project1.title)
+      expect(page).to have_selector('h1:first', text: project1.title)
+    end
 
 
 
@@ -61,7 +62,6 @@ describe "Project Listing" do
       expect(page).to have_selector('.navbar ul li.active a', text: "Projects")
 
 
-    end
 
     end
   end
